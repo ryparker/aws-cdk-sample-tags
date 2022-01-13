9e5634d6de3f927ec65b21acd7e792508655f2ac
Ryan Parker <ryan.parker3@outlook.com>
2022-01-13 14:41:54 -0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App, Stack, Tags } from "aws-cdk-lib";
+import { SecurityGroup, Vpc } from "aws-cdk-lib/aws-ec2";
 
 const app = new App();
-const stack = new Stack(app, 'sample');
+const stack = new Stack(app, "test-tags");
+
+const securityGroup = new SecurityGroup(stack, "TempSecurityGroup", {
+  vpc: new Vpc(stack, "TempVpc"),
+});
+
+// Tags.of(stack).add("Environment", "UAT"); // Works just fine
+Tags.of(securityGroup).add("Environment", "UAT"); // Works just fine
